973ef1df714d8ce2a842b0aee45cdaebea46cac0
sacret2 <mrbartosiak@gmail.com>
2020-01-28 08:02:24 +0100

    initial commit

---
+++ src/main/java/com/mar/toolshare/ToolshareApplication.java
@@ -0,0 +1,13 @@
+package com.mar.toolshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mar/toolshare/ToolshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mar.toolshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
